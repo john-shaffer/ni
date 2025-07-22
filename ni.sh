445e08d2bbbfc73c90feb98cdfcfa539bb47b364
John Shaffer <jdsha@pm.me>
2025-07-22 13:30:51 -0500

    Fix typo (bootstrapping)

--- ni.sh
+++ ni.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo
-echo Bootstraping the NI installer, hold on...
+echo Bootstrapping the NI installer, hold on...
 echo
 nix-env -iA nixos.diceware nixos.wget 
 wget -q https://raw.githubusercontent.com/john-shaffer/ni/master/install.sh -O /etc/nixos/install.sh && chmod +x /etc/nixos/install.sh
